c757037e3a02eda2b3c5355a7942834234619ad6
engineerJPark <jshackist@DESKTOP-EGRL9H9.localdomain>
2021-08-22 00:46:39 +0900

    lesson-5

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+cmake -DGLFW_BUILD_DOCS=OFF -DUSE_ADDER=OFF -S . -B out/build
--- main.cpp
+++ main.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
-#include <adder.h>
 #include <GLFW/glfw3.h>
 #include <OLASConfig.h>
+#ifdef USE_ADDER
+   #include <adder.h>
+#endif
+
 using namespace std;
  
  
 int main(int argc, char * argv[]){
    cout << "Hello" << '\n';
-   cout << add(72.1, 73.8) << '\n';
-   
+
+   #ifdef USE_ADDER
+      cout << "adder.h is included..." << add(72.1, 73.8) << '\n';
+   #else
+      cout << "adder.h is not included..." << 72.1f + 73.8f << '\n';
+   #endif
    cout << argv[0] << "-Version" << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << '\n';
    
-   GLFWwindow *window;
+   // GLFWwindow *window;
  
-   if( !glfwInit() ){
-      fprintf(stderr, “Failed to initialize GLFW\n”);
-      exit(EXIT_FAILURE);
-   }
- 
-   window = glfwCreateWindow(300,300,”GEARS”,NULL,NULL);
-   if( !window ){
-      fprintf(stderr, “Failed to open GLFW window\n”);
-      exit(EXIT_FAILURE);
-   }
- 
-   while( !glfwWindowShouldClose(window) ){
-      // Draw Gears
-      draw();
- 
-      // Update animation
-      animate();
- 
-      // Swap buffers
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-   }
-   // Terminate GLFW
-   glfwTerminate();
+   //  if( !glfwInit() )
+   //  {
+   //      fprintf( stderr, "Failed to initialize GLFW\n" );
+   //      exit( EXIT_FAILURE );
+   //  }
+
+   // window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+   //  if (!window)
+   //  {
+   //      fprintf( stderr, "Failed to open GLFW window\n" );
+   //      glfwTerminate();
+   //      exit( EXIT_FAILURE );
+   //  }
+
+   //      // Main loop
+   //  while( !glfwWindowShouldClose(window) )
+   //  {
+   //      // Swap buffers
+   //      glfwSwapBuffers(window);
+   //      glfwPollEvents();
+   //  }
+
+   //  // Terminate GLFW
+   //  glfwTerminate();
  
    return 0;
 }
